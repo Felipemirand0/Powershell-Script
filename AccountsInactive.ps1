f0589db51dd9be158c381fd85e87b233a7cecc79
Felipe miranda Nacimento <32574653+Felipemirand0@users.noreply.github.com>
2017-10-18 20:55:36 -0200

    Inactive AD accounts
    Disable Inactive AD accounts that have more than 90 days.

---
+++ AccountsInactive.ps1
@@ -0,0 +1,11 @@
+﻿
+$date= Get-Date -Format dd-MMMM-yyyy 
+$path= "C:\"   
+ 
+#Export List  
+
+search-adaccount -usersonly -accountinactive -timespan "90" | Where-Object {$_.lastlogondate -cgt 0} | select name, LastLogonDate, enabled | Out-File $path\Accountsdisabled $date .txt 
+  
+#Disable accounts on AD   
+ 
+search-adaccount -usersonly -accountinactive -timespan "90" | Where-Object {$_.lastlogondate -cgt 0} | Disable-ADAccount 
