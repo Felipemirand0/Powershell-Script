43f2fa095d82da6d89a2ddb5e716cd0688387fe5
Felipe miranda Nacimento <32574653+Felipemirand0@users.noreply.github.com>
2017-10-18 20:48:35 -0200

    Send a email with Powershell

---
+++ SendEmailPS.ps1
@@ -0,0 +1,20 @@
+﻿$date2 = Get-Date -Format d-MMMM-yyyy 
+
+#SMTP configs
+
+$SMTPClient = New-Object System.Net.Mail.SMTPClient
+$Credentials = new-object System.Net.networkCredential
+$Credentials.domain = "Dominio.ad.com"
+$SmtpClient.Credentials = $Credentials
+$SmtpClient.Host = "mail.DOMINIO.com"
+
+
+$SmtpClient.Send("EMAILSNED@DOMINIO.COM","EMAILRECEIVE@DOMINIO.COM","[Subtitle] - SUCCESS ($date2)", "
+ 
+ Write your Text
+ 
+ $path\Accountsdisabled $date .txt
+
+")
+
+write-host "Email Sent"
